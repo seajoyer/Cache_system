8b45f69d2bd3355c862c4ca25e34023e8e4325f3
seajoyer <imgarison@gmail.com>
2024-12-09 21:36:50 +0300

    lol

--- include/cache/pybind_utils.hpp
+++ include/cache/pybind_utils.hpp
@@ -3,30 +3,5 @@
 #include <pybind11/pybind11.h>
 #include <experimental/optional>
 
-namespace pybind11 { namespace detail {
-    template <typename T>
-    struct type_caster<std::experimental::optional<T>> {
-        using value_type = std::experimental::optional<T>;
-        using T_caster = make_caster<T>;
-
-        static handle cast(const value_type &src, return_value_policy policy, handle parent) {
-            if (!src) return none().release();
-            return T_caster::cast(*src, policy, parent);
-        }
-
-        bool load(handle src, bool convert) {
-            if (src.is_none()) {
-                value = std::experimental::nullopt;
-                return true;
-            }
-            T_caster loader;
-            if (loader.load(src, convert)) {
-                value = loader.operator T&();
-                return true;
-            }
-            return false;
-        }
-
-        PYBIND11_TYPE_CASTER(value_type, _("Optional[") + T_caster::name + _("]"));
-    };
-}}
+// Note: std::experimental::optional type_caster is already provided by pybind11/stl.h
+// No need to define our own type_caster
